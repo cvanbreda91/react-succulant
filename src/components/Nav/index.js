de3b7c08afedaee271d3dec14560c7fdd5e6ae3a
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-11-14 20:54:59 -0500

    rendering

---
+++ src/components/Nav/index.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Nav() {
+    return (
+<h2>hello!</h2>)
+}
+
+export default Nav;
---
+++ src/components/Project/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Project() {
+
+}
+
+export default Project;
